cfe8dcea01a92021132e6568dfecfa6ccdda1a76
Likhitha-Sai-Rajarajeswari-Thulluri <91083096+Likhitha-Sai-Rajarajeswari-Thulluri@users.noreply.github.com>
2021-10-22 08:06:48 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,34 @@
 #include "stats.h"
 
+struct computed
+{
+    float average;
+    float max;
+    float min;
+    computed()
+    {
+        average = 0;
+        max = 0;
+        min = 0;
+    }
+};
+
 Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+    computed obj;
+    float sum = 0;
+    for(float vec:src)
+    {
+        if(obj.max<vec)
+            obj.max = vec;
+        if(obj.min>vec)
+        {
+            obj.min = vec;
+        }
+        sum+=vec;
+    }
+    if(src.size())
+    {
+        obj.average = sum/src.size();
+    }
+    return obj;
 }
