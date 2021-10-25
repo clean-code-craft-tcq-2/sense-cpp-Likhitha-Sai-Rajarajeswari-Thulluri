e722814d438aea237c0bdfe6b9b467e88af04c37
Likhitha-Sai-Rajarajeswari-Thulluri <91083096+Likhitha-Sai-Rajarajeswari-Thulluri@users.noreply.github.com>
2021-10-25 17:43:34 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,42 @@
 #include <vector>
 
+struct Stats
+{
+	float average;
+	float max;
+	float min;
+	Stats()
+	{
+		average = 0;
+		max = 0;
+		min = 0;
+	}
+};
+
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+    Stats ComputeStatistics(const std::vector<float>& );
 }
+
+class IAlerter
+{
+   public:
+   IAlerter();
+
+   bool emailSent;
+   bool ledGlows;
+};
+class EmailAlert: public IAlerter
+{
+};
+class LEDAlert: public IAlerter
+{
+};
+class StatsAlerter
+{
+   public:
+   StatsAlerter(float maxThreshold, std::vector<IAlerter*> vecIAlerter);
+   void checkAndAlert(const std::vector<float>& vecData);
+   private:
+   float m_maxThreshold;
+   std::vector<IAlerter*> m_vecIAlerter;
+};
